d0d9ae53c01e728839ca3ff65b94e5eb479ee4e8
cchumacero <cnchumacero@gmail.com>
2025-02-19 16:09:44 -0300

    Add database configuration file

---
+++ db/config.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Creamos el motor
+engine = create_engine(os.getenv("DATABASE_URL"),
+                    connect_args={"check_same_thread": False}
+                    )
+
+# Luego creamos los parametros para las sessiones que se creen de dicho motor
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+
+# Creamos el mapeador ORM 
+Base = declarative_base()
+
+ # Creamos la función para el uso de session de la DB
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
