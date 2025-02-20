299fccfc2d83c2c890262803bd79b9ae3f6969b6
cchumacero <cnchumacero@gmail.com>
2025-02-20 12:31:13 -0300

    Add product router to main file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import models as model
 from routers.categories import router as category_routes
+from routers.products import router as product_routes
 
 
 
 app = FastAPI()
 app.include_router(router = category_routes, tags=["Categories"], prefix="/categories")
+app.include_router(router = product_routes, tags=["Products"], prefix="/products")
 @app.get("/")
 def root():
     return {"message": "Hello World"}
