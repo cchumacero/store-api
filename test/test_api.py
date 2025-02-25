de40ab417cd8b57e52fa9125fb87d574ea08b18d
cchumacero <cnchumacero@gmail.com>
2025-02-25 13:17:05 -0300

    Add category router tests

--- test/test_api.py
+++ test/test_api.py
@@ -6,6 +6,7 @@ from config.db import get_db, Base
 from main import app
 from dotenv import load_dotenv
 import os
+from crud import category
 
 load_dotenv()
 
@@ -17,10 +18,13 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 def db_session():
     """Fixture para manejar la sesión de la base de datos en cada test."""
     Base.metadata.create_all(bind=engine)
-    session = TestingSessionLocal()
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
     yield session
-    session.rollback()
     session.close()
+    transaction.rollback()
+    connection.close()
 
 @pytest.fixture(scope="function")
 def client(db_session):
@@ -44,12 +48,60 @@ def test_create_category(client, db_session):
     response = client.post(
         "/categories", json={"name": "Test Category", "image": "Test Image"}
     )
-    assert response.status_code == 201, response.text
-    data = response.json()
-    assert data["name"] == "Test Category"
-    assert data["image"] == "Test Image"
+    assert response.status_code == 201
+    _category = category.get_category_by_name(db_session, "Test Category")
+    assert _category is not None
+    assert _category.image == "Test Image"
 
-    from models import Category
-    category = db_session.query(Category).filter_by(name="Test Category").first()
-    assert category is not None
-    assert category.image == "Test Image"
+def test_get_categories(client):
+    client.post(
+        "/categories", json={"name": "Test Category 1", "image": "Test Image 1"}
+    )
+    client.post(
+        "/categories", json={"name": "Test Category 2", "image": "Test Image 2"}
+    )
+    
+    response = client.get("/categories")
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 2
+    assert data[0]["name"] == "Test Category 1"
+    assert data[0]["image"] == "Test Image 1"
+    assert data[1]["name"] == "Test Category 2"
+    assert data[1]["image"] == "Test Image 2"
+    
+
+def test_update_category(client, db_session):
+    post_response = client.post(
+        "/categories", json={"name": "Test Category 1", "image": "Test Image 1"}
+    )
+    assert post_response.status_code == 201
+    _category = post_response.json()
+    
+    put_response = client.put(
+        f"/categories/{_category["id"]}", json={"name": "Updated Test Category 1", "image": "Updated Test Image 1"}
+    )
+    
+    assert put_response.status_code == 200
+    _updated_category = put_response.json()
+    assert _updated_category["name"] == "Updated Test Category 1"
+    assert _updated_category["image"] == "Updated Test Image 1"
+    
+def test_remove_category(client, db_session):
+    post_response = client.post(
+        "/categories", json={"name": "Test Category 1", "image": "Test Image 1"}
+    )
+    assert post_response.status_code == 201
+    _category = post_response.json()
+    
+    delete_response = client.delete(
+        f"/categories/{_category["id"]}"
+    )
+    
+    assert delete_response.status_code == 200
+    _deleted_category = category.get_category_by_id(db_session, _category["id"])
+    assert _deleted_category == None
+    
+    
+    
+    
