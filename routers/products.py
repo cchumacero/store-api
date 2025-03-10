edb7d389fd0ab63bc20e503adc052208d9d9b39e
cchumacero <cnchumacero@gmail.com>
2025-03-10 13:27:27 -0300

    Format products file

--- routers/products.py
+++ routers/products.py
@@ -16,8 +16,10 @@ async def create_product(request: ProductSchema, db: Session = Depends(get_db)):
 @router.get("/")
 async def get_products(
     title: Optional[str] = Query(None, description="Filtrar por título"),
-    min_price: Optional[float] = Query(None, description="Filtrar por precio mínimo"),
-    max_price: Optional[float] = Query(None, description="Filtrar por precio máximo"),
+    min_price: Optional[float] = Query(
+        None, description="Filtrar por precio mínimo"),
+    max_price: Optional[float] = Query(
+        None, description="Filtrar por precio máximo"),
     category: Optional[str] = Query(None, description="Filtrar por categoría"),
     skip: int = 0,
     limit: int = 100,
@@ -38,25 +40,26 @@ async def get_products(
 async def get_product_by_id(product_id: str, db: Session = Depends(get_db)):
     _product = product.get_product_by_id(db, product_id)
     if _product is None:
-        raise HTTPException(status_code= 404, detail="Product Not Found")
+        raise HTTPException(status_code=404, detail="Product Not Found")
     return _product
-        
 
 @router.put("/{product_id}")
-async def update_product(product_id: str, request: ProductSchema, db:Session = Depends(get_db)):
+async def update_product(product_id: str, request: ProductSchema, db: Session = Depends(get_db)):
     try:
-        _product = product.update_product(db, product_id, request.title, request.price, request.description, request.category, request.images)
+        _product = product.update_product(
+            db, product_id, request.title, request.price, request.description, request.category, request.images)
         return _product
     except Exception as e:
         # return Response(status="bad", code=304, message="the updated gone wrong")
-        raise HTTPException(status_code= 404, detail="the updated gone wrong, not modified")
+        raise HTTPException(
+            status_code=404, detail="the updated gone wrong, not modified")
 
 @router.delete("/{product_id}")
-async def remove_product(product_id: str, db:Session = Depends(get_db)):
+async def remove_product(product_id: str, db: Session = Depends(get_db)):
     try:
         _product = product.remove_product(db, product_id)
         return _product
     except Exception as e:
         # return Response(status="bad", code=304, message="the updated gone wrong")
-        raise HTTPException(status_code= 404, detail="the deleted gone wrong, not deleted")
-
+        raise HTTPException(
+            status_code=404, detail="the deleted gone wrong, not deleted")
