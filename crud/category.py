5e8c1850a4299cee7ef59d6bc73d00e2fa2b7fcd
cchumacero <cnchumacero@gmail.com>
2025-03-10 13:30:37 -0300

    Create a class for crud functions of categories

--- crud/category.py
+++ crud/category.py
@@ -3,36 +3,40 @@ from models import Category
 from schemas import CategorySchema
 import uuid
 
-def get_category(db:Session, skip:int=0, limit:int=100):
-    return db.query(Category).offset(skip).limit(limit).all()
+class CategoryRepository:
+    def __init__(self, db: Session):
+        self.db = db
 
-def get_category_by_id(db:Session, category_id:str):
-    return db.query(Category).filter(Category.id == category_id).first()
+    def get_category(self, skip: int = 0, limit: int = 100):
+        return self.db.query(Category).offset(skip).limit(limit).all()
 
-def get_category_by_name(db:Session, category_name:str):
-    return db.query(Category).filter_by(name=category_name).first()
+    def get_category_by_id(self, category_id: str):
+        return self.db.query(Category).filter(Category.id == category_id).first()
 
-def create_category(db:Session, category:CategorySchema):
-    _category = Category(
-        id = str(uuid.uuid4()),
-        name = category.name,
-        image = category.image
-    )
-    db.add(_category)
-    db.commit()
-    db.refresh(_category)
-    return _category
+    def get_category_by_name(self, category_name: str):
+        return self.db.query(Category).filter_by(name=category_name).first()
 
-def remove_category(db:Session, category_id: str):
-    _category = get_category_by_id(db, category_id)
-    db.delete(_category)
-    db.commit()
-    return _category
+    def create_category(self, category: CategorySchema):
+        _category = Category(
+            id=str(uuid.uuid4()),
+            name=category.name,
+            image=category.image
+        )
+        self.db.add(_category)
+        self.db.commit()
+        self.db.refresh(_category)
+        return _category
 
-def update_category(db:Session, category_id: str, name: str, image: str):
-    _category = get_category_by_id(db, category_id)
-    _category.name = name
-    _category.image = image
-    db.commit()
-    db.refresh(_category)
-    return _category
+    def remove_category(self, category_id: str):
+        _category = self.get_category_by_id(category_id)
+        self.db.delete(_category)
+        self.db.commit()
+        return _category
+
+    def update_category(self, category_id: str, name: str, image: str):
+        _category = self.get_category_by_id(category_id)
+        _category.name = name
+        _category.image = image
+        self.db.commit()
+        self.db.refresh(_category)
+        return _category
