eadb033b2f64785645952b4ffe686d517b15145a
cchumacero <cnchumacero@gmail.com>
2025-03-11 16:10:00 -0300

    Add database settings

--- config/db.py
+++ config/db.py
@@ -6,8 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Creamos el motor
-engine = create_engine(os.getenv("DATABASE_URL"))
-
+if os.getenv("ENVIROMENT") == "development":
+    engine = create_engine(os.getenv("DATABASE_URL"))
+else:
+    engine = create_engine(os.getenv("POSTGRES_URL"))
+    
 # Luego creamos los parametros para las sessiones que se creen de dicho motor
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
